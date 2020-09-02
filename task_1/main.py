21d0a3a324e165362fe7d6de673582cd2f01807e
Andrew Dolgov <andruxa-dusha@yandex.ru>
2020-09-02 23:29:13 +0700

    Выполнил Задание 1

---
+++ task_1/main.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("https://www.software-testing.ru/")
