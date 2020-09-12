0eae8e44cfb2e026d7a012034a5f0d3ed6a36a9a
Andrew Dolgov <andruxa-dusha@yandex.ru>
2020-09-12 19:39:25 +0700

    Запуск разных браузеров

--- task_4/browsers.py
+++ task_4/browsers.py
@@ -6,22 +6,23 @@ from msedge.selenium_tools import Edge, EdgeOptions
 @pytest.fixture
 def driver(request):
     # старая схема запуска Firefox:
-    # options = webdriver.FirefoxOptions()
-    # options.binary_location = "C:\Program Files\Mozilla Firefox ESR\firefox.exe"
     # wd = webdriver.Firefox(capabilities={"marionette": False})
 
     # запуск Firefox Nightly:
     # options = webdriver.FirefoxOptions()
-    # options.binary_location = "C:\Program Files\Firefox Nightly\firefox.exe"
-    # wd = webdriver.Firefox()
+    # options.binary_location = "C:\\Program Files\Firefox Nightly\\firefox.exe"
+    # wd = webdriver.Firefox(options=options)
+
+    # запуск Firefox (альтернативный вариант указания пути):
+    wd = webdriver.Firefox(firefox_binary="C:\\Program Files\Mozilla Firefox\\firefox.exe")
 
     # Microsoft Edge (based on Chromium)
     # https://github.com/microsoft/edge-selenium-tools
     # https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
-    options = EdgeOptions()
-    options.use_chromium = True
-    wd = Edge(options=options, capabilities={"unexpectedAlertBehaviour": "dismiss"})
-    print(wd.capabilities)
+    # options = EdgeOptions()
+    # options.use_chromium = True
+    # wd = Edge(options=options, capabilities={"unexpectedAlertBehaviour": "dismiss"})
+    # print(wd.capabilities)
 
     request.addfinalizer(wd.quit)
     return wd
